779278ee18e958eb3b9d0060a875860f8d329b6c
Anuja Gunasinghe <a.r.gunasinghe@gmail.com>
2025-03-18 20:15:01 +0530

    cursor skeleton

--- components/previews/sticky-cursor-preview.tsx
+++ components/previews/sticky-cursor-preview.tsx
@@ -1,9 +1,14 @@
+import { MenuIcon } from "lucide-react";
 import StickyCursor from "../showcase/sticky-cursor/sticky-cursor";
 
 export default function StickyCursorPreview() {
   return (
     <div>
-      <StickyCursor />
+      <StickyCursor>
+        <header className="place-items-center">
+          <MenuIcon />
+        </header>
+      </StickyCursor>
     </div>
   );
 }
--- components/showcase/sticky-cursor/sticky-cursor.tsx
+++ components/showcase/sticky-cursor/sticky-cursor.tsx
@@ -1,3 +1,14 @@
-export default function StickyCursor() {
-  return <div></div>;
+import styles from "./style.module.css";
+
+interface StickyCursorProps {
+  children: React.ReactNode;
+}
+
+export default function StickyCursor({ children }: StickyCursorProps) {
+  return (
+    <div>
+      <div className={styles.cursor}></div>
+      {children}
+    </div>
+  );
 }
